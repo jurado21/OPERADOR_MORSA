dbb8be165108eb80e0c21e26857bbe73e46ccf3f
unknown <abel.jurado210399@gmail.com>
2024-06-03 11:20:03 -0500

    morsa

---
+++ PRUEBA.PY/trabajo2.py
@@ -0,0 +1,14 @@
+#entrada de datos
+numero_magico= (1,2,3,4,5.6,7,9)
+
+#leer po pantalla numero usuario
+numero_usuario:int(input("introdusca el numero que sea entre 1 y 9"))
+
+#multiplica el numero usuario por si mismo
+numero_usuario=(numero_usuario*9)
+
+#multiplicacion del numeri magico por el numero usuario
+numero_magico=(numero_magico*numero_usuario)
+
+#finalmente muestra el valor del numero magico
+print("numero magico")
